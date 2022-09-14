c57da6b6d4cb6a6bd8ceb75981248ffa33888bee
AcademyXiTech <112535920+AcademyXiTech@users.noreply.github.com>
2022-09-14 20:08:08 +1000

    Add files via upload

---
+++ M3_Blog/M3_Blog/M3_BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3_BlogApp.swift
+//  M3_Blog
+//
+//  Created by Cameron Edwards on 14/9/22.
+//
+
+import SwiftUI
+
+@main
+struct M3_BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
